ecbec43ef6c6569c2fb79e2789472ea9aa80a18d
bizexo <41581712+bizexo@users.noreply.github.com>
2019-10-18 09:20:46 +0530

    Updates

---
+++ precache-manifest.b2146e4bc5a56533f9d4f88143a36cab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "032d9fbf36088629d7d6e12c80cd1133",
+    "url": "/krushita-agro/index.html"
+  },
+  {
+    "revision": "3a73aecae50ff9e2cec3",
+    "url": "/krushita-agro/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "519a34e5ac610d7aee10",
+    "url": "/krushita-agro/static/js/2.3283c06b.chunk.js"
+  },
+  {
+    "revision": "3a73aecae50ff9e2cec3",
+    "url": "/krushita-agro/static/js/main.816da1c0.chunk.js"
+  },
+  {
+    "revision": "8d704e44eec12292fcac",
+    "url": "/krushita-agro/static/js/runtime-main.61b32972.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/krushita-agro/static/media/logo.25bf045c.svg"
+  }
+]);
