46c136798a70fb7b0493d24bc654009952f9957d
bizexo <41581712+bizexo@users.noreply.github.com>
2019-11-12 23:09:14 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import logo from './logo.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        We are coming soon................
-        </a>
-      </header>
-    </div>
+     <form className="form-signin">
+       <img src={logo} className="App-logo" alt="logo" />
+       <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus/>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required/>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"/> Remember me
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+          </form>
+</div>
   );
 }
 
